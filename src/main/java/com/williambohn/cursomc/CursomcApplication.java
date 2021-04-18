d7859d8c411835176fff4d85cfe48d12303c0dc7
William Bohn <williamxavero@gmail.com>
2021-04-18 18:34:05 -0300

    Rest foi testado

--- src/main/java/com/williambohn/cursomc/CursomcApplication.java
+++ src/main/java/com/williambohn/cursomc/CursomcApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
