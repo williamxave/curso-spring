dede83e737ae67675d6aeb5918837cbc3fd8753e
William Bohn <williamxavero@gmail.com>
2021-04-19 18:48:59 -0300

    Criando operacao de intanciacao

--- src/main/java/com/williambohn/cursomc/CursomcApplication.java
+++ src/main/java/com/williambohn/cursomc/CursomcApplication.java
@@ -1,11 +1,37 @@
 package com.williambohn.cursomc;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.williambohn.cursomc.domain.Categoria;
+import com.williambohn.cursomc.repository.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	private List<Categoria> saveAll;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório" );
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		
+	}
+
+
+
 }
